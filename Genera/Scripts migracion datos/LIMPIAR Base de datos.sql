593679861ee143db24579d4a3625e112a98eb91b
FRCayetano <gaetan.collet@outlook.com>
2015-06-29 18:07:21 +0200

    Script para limpiar la base de datos antes de recrear toda la estructura

---
+++ Genera/Scripts migracion datos/LIMPIAR Base de datos.sql
@@ -0,0 +1,28 @@
+--Borrar la configuracion actual
+delete from Pers_Proyectos_Agencias_Ingresos
+delete from Pers_Proyectos_Agencias_Gastos
+delete from Pers_Presupuestos_Equipos_Proyectos
+delete from Proyectos
+delete from Pers_Equipos
+
+--Borrar los Ingresos y Gastos existentes
+delete from Pers_IngresoAgencia_Cab
+delete from Pers_GastosAgencia_Cab
+
+--Desabilitar el trigger Deleted de la tabla Almacen_Hist_Mov antes de borrar
+disable trigger [dbo].[Almacen_Hist_Mov_DTrig] ON [dbo].[Almacen_Hist_Mov]
+
+delete from Almacen_Hist_Mov
+
+enable trigger [dbo].[Almacen_Hist_Mov_DTrig] ON [dbo].[Almacen_Hist_Mov]
+
+--Borrar cabecera de pedidos proveedores y clientes
+delete from Pedidos_Cli_Cabecera
+delete from Pedidos_Prov_Cabecera
+
+--Borrar los datos de clientes y proveedores
+delete from Clientes_Datos where idCliente <> 0
+delete from Prov_datos where idProveedor <> 0
+
+
+--A ver lo que hacemos con los equipos
